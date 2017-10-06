7ecf2f1389458616273a5855eb658e9601ee7655
Denis <32293591+Duskaev@users.noreply.github.com>
2017-10-06 15:33:51 +0300

    Update task_02_03.py

--- task_02_03.py
+++ task_02_03.py
@@ -1,3 +1,3 @@
 def average(lst):
 	resultat=sum(lst)/len(lst) #Вычесляем среднеарефметичекое сумма чисел/на их количество
-	retrum round(resultat, 3) #округляем до 3 знака после точки
+	return round(resultat, 3) #округляем до 3 знака после точки
