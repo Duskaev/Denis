551492b6488e35dbfa71e7b1966367673acdbd05
Denis <32293591+Duskaev@users.noreply.github.com>
2017-10-06 15:32:59 +0300

    Update task_02_01.py
    подправил

--- task_02_01.py
+++ task_02_01.py
@@ -2,6 +2,6 @@ def is_palindrome(s):
     palindrome=str(s).lower(s).replace(' ','').replace(',','') #Преобразуем все в строку, приводим все к нижнему регистру, удалаем пробелы и запятые
     palindrome=palindrome1[::-1] #вводим еще одну переменную для сравнения
     if palindrome==palindrome1: #сравниваем наши переменные
-        retrum True
+        return True
     else:
-        retrum False
+        return False
