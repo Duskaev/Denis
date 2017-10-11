cba345b011006c7ffca8a4718b5d5163b5fac851
Denis <32293591+Duskaev@users.noreply.github.com>
2017-10-11 14:20:29 +0300

    Update task_01_02.py

--- task_01_02.py
+++ task_01_02.py
@@ -1,13 +1,9 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if a<=b and a<=c:
-	if b<=c:
-	    print(a,b,c)
-	elif c<=b:
-	    print(a,c,b)
-elif b<=a and b<=c:
-	if c<=a:
-	    print(b,c,a)
-	elif a<=c:
-	    print(b,a,c)
+kol_tarelok=int(input())
+kol_sredstva=float(input())
+kol_vimitih=kol_sredstva/0.5 # сколько тарелок хватит средства
+if kol_tarelok>kol_sredstva:
+		Print(int(kol_tarelok-kol_vimitih))
+elif kol_tarelok<kol_sredstva:
+		Print(float((kol_vimitih-kol_tarelok)*0.5))
+else:
+print('Все тарелки вымыты, средство закончилось')
